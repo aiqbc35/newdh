59d1001cc9dad9845084310dc99ee5b2fb2d82d1
rookie <rookie@rookiedeMacBook-Pro.local>
2018-03-27 22:24:24 +0800

    create html

--- app/view/index/mune.blade.php
+++ app/view/index/mune.blade.php
@@ -1,5 +1,5 @@
 <div class="mina">
-    <a href="/index/signIn" target="_blank">自助申请</a>&nbsp;|
-    <a href="/index/updateUrl" target="_blank">自助管理</a>&nbsp;|
-    <a href="/index/subscribe" target="_blank">地址发布页</a>
+    <a href="/sign.html" target="_blank">自助申请</a>&nbsp;|
+    <a href="/updateurl.html" target="_blank">自助管理</a>&nbsp;|
+    <a href="/subscribe.html" target="_blank">地址发布页</a>
 </div>
